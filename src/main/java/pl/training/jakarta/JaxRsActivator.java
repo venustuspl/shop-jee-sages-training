b109dd2978360a95e796c43cb49b74e6c85f6f08
Marcin Chrost <mchrost82@gmail.com>
2022-12-20 21:48:48 +0100

    full cleanup

--- src/main/java/pl/training/jakarta/JaxRsActivator.java
+++
@@ -1,9 +0,0 @@
-package pl.training.jakarta;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class JaxRsActivator extends Application {
-
-}
